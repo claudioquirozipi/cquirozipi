1b2c1d2bbc7f8380e301016cbd6b98e100e0ace3
Claudio Quiroz <claudioquirozipi@gmail.com>
2022-04-07 17:44:52 -0500

    add data to home

---
+++ content/home/home-2.md
@@ -0,0 +1,10 @@
+---
+title: Home 2
+home_image: img/174848.png
+description: |-
+  descripción 
+
+  * ul 11
+  * ul 22
+url: https://cquirozipi.firebaseapp.com/
+---
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,12 @@ import type { NextPage, GetStaticProps } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { Remarkable } from "remarkable";
+let md = new Remarkable();
+// import marked from "marked";
 interface Props {
   content: { attributes: HomeAttributes };
 }
@@ -11,7 +16,9 @@ interface HomeAttributes {
   hero_description: string;
   hero_image: string;
 }
-const Home: NextPage<Props> = (props) => {
+const Home: NextPage<any> = (props) => {
+  const { home } = props;
+  console.log("props", props);
   // const { hero_title, hero_description, hero_image } = props.content.attributes;
   return (
     <div className={styles.container}>
@@ -21,6 +28,18 @@ const Home: NextPage<Props> = (props) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>hola mundo</h1>
+      {JSON.stringify(home)}
+      {home.map((d: any, i: number) => (
+        <div key={i}>
+          <h1>{d.data.title}</h1>
+          <div
+            dangerouslySetInnerHTML={{ __html: md.render(d.data.description) }}
+          ></div>
+          <p>{md.render(d.data.description)}</p>
+          <img src={d.data.home_image} alt="" />
+          <a href={d.data.url}>link</a>
+        </div>
+      ))}
       {/* <h1>{hero_title}</h1>
       <p>{hero_description}</p>
       <p>{hero_image}</p> */}
@@ -28,9 +47,31 @@ const Home: NextPage<Props> = (props) => {
     </div>
   );
 };
-// export const getStaticProps: GetStaticProps = async () => {
-//   const content = await import(`../content/pages/${"home"}.md`);
-//   return { props: { content: content.default } };
-// };
+export const getStaticProps: GetStaticProps = async () => {
+  let props = {};
+  const homeFiles = fs.readdirSync(path.join("content/home"));
+  console.log("files", homeFiles);
+
+  const home = homeFiles.map((fileName) => {
+    const slug = fileName.replace(".md", "");
+
+    const markDownWithMeta = fs.readFileSync(
+      path.join("content/home", fileName),
+      "utf-8"
+    );
+    const { data } = matter(markDownWithMeta);
+    return {
+      slug,
+      data,
+    };
+  });
+
+  console.log("home", home);
+  return {
+    props: {
+      home,
+    },
+  };
+};
 
 export default Home;
