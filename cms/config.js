7809935b92980a6ab86bd8a44def489325150f90
Claudio Quiroz <claudioquirozipi@gmail.com>
2022-04-06 19:01:57 -0500

    test

---
+++ cms/config.js
@@ -0,0 +1,41 @@
+export default {
+  cms_manual_init: true,
+  backend: {
+    //   name: 'github',
+    name: "test-repo",
+    repo: "claudioquirozipi/cquirozipi",
+    branch: "main",
+  },
+  media_folder: "public/img",
+  public_folder: "img",
+  collections: [
+    {
+      name: "pages",
+      label: "Pages",
+      files: [
+        {
+          label: "Home",
+          name: "home",
+          file: "content/pages/home.md",
+          fields: [
+            {
+              label: "Hero Title",
+              name: "hero_title",
+              widget: "string",
+            },
+            {
+              label: "Hero Description",
+              name: "hero_description",
+              widget: "markdown",
+            },
+            {
+              label: "Hero Image",
+              name: "hero_image",
+              widget: "image",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
+  // reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.md$/,
+      loader: 'frontmatter-markdown-loader',
+    });
+    return config;
+  },
 }
