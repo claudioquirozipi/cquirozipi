711b2326c0bda54bb6539ed129b85ffc9e5bd6ab
Claudio Quiroz <claudioquirozipi@gmail.com>
2022-04-07 00:49:51 -0500

    config.yml and config proyect

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,31 +1,4 @@
-import dynamic from "next/dynamic";
-import { createElement as h } from "react";
-import config from "../cms/config";
-import CMS from "netlify-cms-app";
-
-// const CMS = dynamic(
-//   (): any =>
-//     import("netlify-cms-app").then((cms: any) => {
-//       cms.init({ config });
-//     }),
-//   { ssr: false, loading: () => <p>Loading...</p> }
-// );
-// CMS.init();
-// const MyTemplate = h("div", {}, "my template test");
-// // CMS.registerPreviewTemplate("my-template", MyTemplate);
-
-// const AdminPage: React.FC = () => {
-//   return (
-//     <div>
-//       <h1>hola admin</h1>
-//     </div>
-//   );
-// };
-
-// export default AdminPage;
-
 import { useEffect } from "react";
-// import BlogPostPreview from '../previews/BlogPostPreview'
 
 const Admin = () => {
   useEffect(() => {
