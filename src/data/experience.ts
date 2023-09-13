d13be93dd475ab96559ef589ca14ce3d3f0cf2e1
Claudio Quiroz <claudioquirozipi@gmail.com>
2023-09-13 01:12:24 -0500

    add border

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -1,12 +1,31 @@
 export const experience = [
     {
-        title: 'Software Engineer',
-        company: 'Google',
-        location: 'Mountain View, CA',
-        startDate: '2019-01-01',
-        endDate: '2020-01-01',
-        description: 'I worked on the Google Search team.',
-        logo: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png',
-        url: 'https://google.com',
+        title: 'Fullstack developer',
+        company: 'Nttdata',
+        location: 'Trujillo, Perú',
+        startDate: new Date("2020-09-20"),
+        endDate: new Date(),
+        description: 'Desarrollador de aplicaciones del lado del front y del backnend.',
+        logo: 'https://media.licdn.com/dms/image/D560BAQF3yEz9odOtlw/company-logo_100_100/0/1688376091927?e=1702512000&v=beta&t=3qod3JI71Vg5ujd_tXVw0iECbQ_CYGjzBpLKBgdax3M',
+        url: 'https://www.linkedin.com/company/nttdata/mycompany/verification/',
+    }, 
+    {
+        title: 'Frontend developer',
+        company: 'ID BI',
+        location: 'Lima, Perú',
+        startDate: new Date("2020-01-27"),
+        endDate: new Date("2020-06-30"),
+        description: 'Desarrollé un sistema de inventarios para restaurantes',
+        logo: 'https://media.licdn.com/dms/image/C4E0BAQGCWGYw7IMIbw/company-logo_100_100/0/1676065539172?e=1702512000&v=beta&t=O1rjS_kPKwxs8JxjcK8LDZaAbMqKEGXjmCTpTfhtxKk',
+        url: 'https://www.negocios.idbi.pe/',
+    }, 
+    {
+        title: 'Frontend developer',
+        company: 'Freelance',
+        location: 'Remote',
+        startDate: new Date('2017-01-10'),
+        endDate: new Date('2020-09-20'),
+        description: 'Desarrollé multiples landing pages para diferentes clientes',
+        
     }
 ];
