0a92ae119f7f20cd9bf5951ac332ee51f4936eda
Claudio Quiroz <claudioquirozipi@gmail.com>
2023-09-17 16:39:25 -0500

    update certificates pages

--- src/data/certificates.ts
+++ src/data/certificates.ts
@@ -1,9 +1,18 @@
-export const  certificates = [
+export interface Certificate {
+    title: string;
+    certificates_image: string;
+    description: string;
+    date: Date;
+    institute: string;
+    slug: string;
+    url?: string;
+}
+export const  certificates : Certificate[]= [
     {
       title: 'AWS',
       certificates_image: 'img/aws.jpg',
       description: 'Curso de amazon web services (AWS) desde cero',
-      date: '2020-10-15T00:00:00.101Z',
+      date: new Date('2020-10-15T00:00:00.101Z'),
       institute: 'udemy',
       slug: 'awsudemy'
     },
@@ -11,7 +20,7 @@ export const  certificates = [
       title: 'Firebase',
       certificates_image: 'img/firebaseEDTeam.png',
       description: 'Crear sitios y aplicaciónes webs usando Firebase como backend',
-      date: '2018-10-13T00:00:00.101Z',
+      date: new Date('2018-10-13T00:00:00.101Z'),
       institute: 'EDTeam',
       slug: 'firebase'
     },
@@ -19,7 +28,7 @@ export const  certificates = [
       title: 'Go',
       certificates_image: 'img/diploma-programacion-golang-2020.jpg',
       description: 'Curso práctico de go, creación de un servidor web',
-      date: '2021-11-20T22:17:41.136Z',
+      date: new Date('2021-11-20T22:17:41.136Z'),
       institute: 'platzi',
       slug: 'go'
     },
@@ -27,7 +36,7 @@ export const  certificates = [
       title: 'JavaScript',
       certificates_image: 'img/javaScriptEDTeam.png',
       description: 'JavaScript desde cero',
-      date: '2018-11-21T00:00:00.101Z',
+      date: new Date('2018-11-21T00:00:00.101Z'),
       institute: 'EDTeam',
       slug: 'javascript'
     },
@@ -36,7 +45,7 @@ export const  certificates = [
       url: 'https://platzi.com/p/cquirozipi/curso/1608-aprender-2019/diploma/detalle/',
       certificates_image: 'img/diploma-aprender-2019.jpg',
       description: 'Curso de estrategias para aprender en línea efectivamente',
-      date: '2020-12-25T00:00:00.101Z',
+      date: new Date('2020-12-25T00:00:00.101Z'),
       institute: 'platzi',
       slug: 'platziAprenderLinea'
     },
@@ -45,7 +54,7 @@ export const  certificates = [
       url: 'https://platzi.com/p/cquirozipi/curso/1247-arquitectura-software/diploma/detalle/',
       certificates_image: 'img/diploma-arquitectura-software.jpg',
       description: 'Fundamentos de Arquitectura de software',
-      date: '2021-09-07T00:00:00.101Z',
+      date: new Date('2021-09-07T00:00:00.101Z'),
       institute: 'platzi',
       slug: 'platziArquitecturaSoftware'
     },
@@ -54,7 +63,7 @@ export const  certificates = [
       url: 'https://platzi.com/p/cquirozipi/curso/1937-python/diploma/detalle/',
       certificates_image: 'img/diploma-python.jpg',
       description: 'Curso básico de python',
-      date: '2020-12-31T00:00:00.101Z',
+      date: new Date('2020-12-31T00:00:00.101Z'),
       institute: 'platzi',
       slug: 'platziCursoBasicoPython'
     },
@@ -63,7 +72,7 @@ export const  certificates = [
       url: 'https://platzi.com/p/cquirozipi/curso/1940-finanzas/diploma/detalle/',
       certificates_image: 'img/diploma-finanzas.jpg',
       description: 'Curso de finanzas personales',
-      date: '2020-12-13T00:00:00.101Z',
+      date: new Date('2020-12-13T00:00:00.101Z'),
       institute: 'platzi',
       slug: 'platziFinanzasPersonales'
     },
@@ -72,7 +81,7 @@ export const  certificates = [
       url: 'https://platzi.com/p/cquirozipi/curso/1566-bd/diploma/detalle/',
       certificates_image: 'img/diploma-bd.jpg',
       description: 'Curso de fundamentos para bases de datos',
-      date: '2021-03-13T00:00:00.101Z',
+      date: new Date('2021-03-13T00:00:00.101Z'),
       institute: 'platzi',
       slug: 'platziFundamentosDB'
     },
@@ -81,7 +90,7 @@ export const  certificates = [
       url: 'https://platzi.com/p/cquirozipi/curso/1618-gatsby/diploma/detalle/',
       certificates_image: 'img/diploma-gatsby.jpg',
       description: 'Curso de Gatsby',
-      date: '2021-07-18T00:00:00.101Z',
+      date: new Date('2021-07-18T00:00:00.101Z'),
       institute: 'platzi',
       slug: 'platziGatsby'
     },
@@ -90,7 +99,7 @@ export const  certificates = [
       url: 'https://platzi.com/p/cquirozipi/curso/1557-git-github/diploma/detalle/',
       certificates_image: 'img/diploma-git-github.jpg',
       description: 'Curso profesional de git y github',
-      date: '2020-12-29T00:00:00.101Z',
+      date: new Date('2020-12-29T00:00:00.101Z'),
       institute: 'platzi',
       slug: 'platziGit'
     },
@@ -99,7 +108,7 @@ export const  certificates = [
       url: 'https://platzi.com/p/cquirozipi/course/1846-programacion-golang-2020/diploma/detalle/',
       certificates_image: 'img/diploma-programacion-golang.jpg',
       description: 'Curso básico de programación en go',
-      date: '2021-11-20T00:00:00.101Z',
+      date: new Date('2021-11-20T00:00:00.101Z'),
       institute: 'platzi',
       slug: 'platziGo'
     },
@@ -108,7 +117,7 @@ export const  certificates = [
       url: 'https://platzi.com/p/cquirozipi/curso/2357-golang-intermedio/diploma/detalle/',
       certificates_image: 'img/diploma-golang-intermedio.jpg',
       description: 'Curso de Go intermedio, Programación orientada a objetos y concurrencia',
-      date: '2021-09-119T00:00:00.101Z',
+      date: new Date('2021-09-119T00:00:00.101Z'),
       institute: 'platzi',
       slug: 'platziGoIntermedio'
     },
@@ -117,7 +126,7 @@ export const  certificates = [
       url: 'https://platzi.com/p/cquirozipi/curso/1945-ingles-principiantes/diploma/detalle/',
       certificates_image: 'img/diploma-ingles-principiantes.jpg',
       description: 'Basic english course A1 for beginners',
-      date: '2020-11-26T00:00:00.101Z',
+      date: new Date('2020-11-26T00:00:00.101Z'),
       institute: 'platzi',
       slug: 'platziInglesA1'
     },
@@ -126,7 +135,7 @@ export const  certificates = [
       url: 'https://platzi.com/p/cquirozipi/curso/2196-ingles-basico-descripciones-comparativos/diploma/detalle/',
       certificates_image: 'img/diploma-ingles-basico-descripciones-comparativos.jpg',
       description: 'Basic english course descriptions and comparatives',
-      date: '2021-08-15T00:00:00.101Z',
+      date: new Date('2021-08-15T00:00:00.101Z'),
       institute: 'platzi',
       slug: 'platziInglesBasico'
     },
@@ -135,7 +144,7 @@ export const  certificates = [
       url: 'https://platzi.com/p/cquirozipi/curso/2005-taller-principiantes/diploma/detalle/',
       certificates_image: 'img/diploma-taller-principiantes.jpg',
       description: 'English workshop for beginners',
-      date: '2021-08-04T00:00:00.101Z',
+      date: new Date('2021-08-04T00:00:00.101Z'),
       institute: 'platzi',
       slug: 'platziInglesPrincipiante'
     },
@@ -144,7 +153,7 @@ export const  certificates = [
       url: 'https://platzi.com/p/cquirozipi/curso/1631-java-basico/diploma/detalle/',
       certificates_image: 'img/diploma-java-basico.jpg',
       description: 'Curso de introducción a java SE',
-      date: '2021-01-11T00:00:00.101Z',
+      date: new Date('2021-01-11T00:00:00.101Z'),
       institute: 'platzi',
       slug: 'platziJavaBasico'
     },
@@ -153,7 +162,7 @@ export const  certificates = [
       url: 'https://platzi.com/p/cquirozipi/curso/1629-java-oop/diploma/detalle/',
       certificates_image: 'img/diploma-java-oop.jpg',
       description: 'Curso práctico de go, Creación de un servidor web',
-      date: '2021-05-01T00:00:00.101Z',
+      date: new Date('2021-05-01T00:00:00.101Z'),
       institute: 'platzi',
       slug: 'platziJavaOOP'
     },
@@ -162,7 +171,7 @@ export const  certificates = [
       url: 'https://platzi.com/p/cquirozipi/curso/1996-java-spring/diploma/detalle/',
       certificates_image: 'img/diploma-java-spring.jpg',
       description: 'Curso de java sprint',
-      date: '2021-05-14T00:00:00.101Z',
+      date: new Date('2021-05-14T00:00:00.101Z'),
       institute: 'platzi',
       slug: 'platziJavaSpring'
     },
@@ -171,7 +180,7 @@ export const  certificates = [
       url: 'https://platzi.com/p/cquirozipi/curso/1788-jest/diploma/detalle/',
       certificates_image: 'img/diploma-jest.jpg',
       description: 'Curso de unit testing con jest en react',
-      date: '2021-02-06T00:00:00.101Z',
+      date: new Date('2021-02-06T00:00:00.101Z'),
       institute: 'platzi',
       slug: 'platziJest'
     },
@@ -180,7 +189,7 @@ export const  certificates = [
       url: 'https://platzi.com/p/cquirozipi/curso/1642-javascript-profesional/diploma/detalle/',
       certificates_image: 'img/diploma-javascript-profesional.jpg',
       description: 'Profesional de javascript',
-      date: '2021-05-14T00:00:00.101Z',
+      date: new Date('2021-05-14T00:00:00.101Z'),
       institute: 'platzi',
       slug: 'platziJs'
     },
@@ -189,7 +198,7 @@ export const  certificates = [
       url: 'https://platzi.com/p/cquirozipi/curso/1815-ecmascript-6/diploma/detalle/',
       certificates_image: 'img/diploma-ecmascript-6.jpg',
       description: 'Curso de Ecmascript 6',
-      date: '2020-12-25T00:00:00.101Z',
+      date: new Date('2020-12-25T00:00:00.101Z'),
       institute: 'platzi',
       slug: 'platziJsES6'
     },
@@ -198,7 +207,7 @@ export const  certificates = [
       url: 'https://platzi.com/p/cquirozipi/curso/2069-negocios-data-science/diploma/detalle/',
       certificates_image: 'img/diploma-negocios-data-science.jpg',
       description: 'Análisis de negocios para ciencia de datos',
-      date: '2020-12-20T00:00:00.101Z',
+      date: new Date('2020-12-20T00:00:00.101Z'),
       institute: 'platzi',
       slug: 'platziNegociosDatascience'
     },
@@ -207,7 +216,7 @@ export const  certificates = [
       url: 'https://platzi.com/p/cquirozipi/curso/1991-next/diploma/detalle/',
       certificates_image: 'img/diploma-next.jpg',
       description: 'Curso de next.js',
-      date: '2021-06-20T00:00:00.101Z',
+      date: new Date('2021-06-20T00:00:00.101Z'),
       institute: 'platzi',
       slug: 'platziNextJs'
     },
@@ -216,7 +225,7 @@ export const  certificates = [
       url: 'https://platzi.com/p/cquirozipi/curso/1474-oop/diploma/detalle/',
       certificates_image: 'img/diploma-oop.jpg',
       description: 'Curso de programación orientada a objetos',
-      date: '2021-02-20T00:00:00.101Z',
+      date: new Date('2021-02-20T00:00:00.101Z'),
       institute: 'platzi',
       slug: 'platziOOP'
     },
@@ -225,7 +234,7 @@ export const  certificates = [
       url: 'https://platzi.com/p/cquirozipi/curso/1998-productividad/diploma/detalle/',
       certificates_image: 'img/diploma-productividad.jpg',
       description: 'Curso de productividad',
-      date: '2020-11-24T00:00:00.101Z',
+      date: new Date('2020-11-24T00:00:00.101Z'),
       institute: 'platzi',
       slug: 'platziProductividad'
     },
@@ -234,7 +243,7 @@ export const  certificates = [
       url: 'https://platzi.com/p/cquirozipi/curso/1764-python-cs/diploma/detalle/',
       certificates_image: 'img/diploma-python-cs.jpg',
       description: 'Curso de introducción al pensamiento computacional con python',
-      date: '2021-01-24T00:00:00.101Z',
+      date: new Date('2021-01-24T00:00:00.101Z'),
       institute: 'platzi',
       slug: 'platziPythonPensamiento'
     },
@@ -243,7 +252,7 @@ export const  certificates = [
       url: 'https://platzi.com/p/cquirozipi/curso/2118-react-hooks/diploma/detalle/',
       certificates_image: 'img/diploma-react-hooks.jpg',
       description: 'Curso profesional de react hooks',
-      date: '2021-06-30T00:00:00.101Z',
+      date: new Date('2021-06-30T00:00:00.101Z'),
       institute: 'platzi',
       slug: 'platziReactHooks'
     },
@@ -252,7 +261,7 @@ export const  certificates = [
       url: 'https://platzi.com/p/cquirozipi/curso/1652-react-router-redux/diploma/detalle/',
       certificates_image: 'img/diploma-react-router-redux.jpg',
       description: 'React router redux',
-      date: '2021-01-21T00:00:00.101Z',
+      date: new Date('2021-01-21T00:00:00.101Z'),
       institute: 'platzi',
       slug: 'platziReactRouterRedux'
     },
@@ -261,7 +270,7 @@ export const  certificates = [
       url: 'https://platzi.com/p/cquirozipi/curso/1613-redux/diploma/detalle/',
       certificates_image: 'img/diploma-redux.jpg',
       description: 'Curso de redux por Bedu',
-      date: '2020-12-24T00:00:00.101Z',
+      date: new Date('2020-12-24T00:00:00.101Z'),
       institute: 'platzi',
       slug: 'platziRedux'
     },
@@ -269,7 +278,7 @@ export const  certificates = [
       title: 'Redux React y GraphQl',
       certificates_image: 'img/reduxReactGraphQl.jpg',
       description: 'Aprende Redux con React hooks y GraphQL fácilmente',
-      date: '2020-07-05T00:00:00.101Z',
+      date: new Date('2020-07-05T00:00:00.101Z'),
       institute: 'udemy',
       slug: 'reduxReactGraphQl'
     },
@@ -277,7 +286,7 @@ export const  certificates = [
       title: 'Rxjs',
       certificates_image: 'img/rxjs.jpg',
       description: 'ReactiveX - Rxjs: De cero hasta los detalles',
-      date: '2022-03-20T00:00:00.414Z',
+      date: new Date('2022-03-20T00:00:00.414Z'),
       institute: 'udemy',
       slug: 'rxjs'
     },
@@ -285,7 +294,7 @@ export const  certificates = [
       title: 'Serverless AWS',
       certificates_image: 'img/serverlessAWS.jpg',
       description: 'Serverless en español con AWS y Serverless Framework',
-      date: '2020-05-29T00:00:00.101Z',
+      date: new Date('2020-05-29T00:00:00.101Z'),
       institute: 'udemy',
       slug: 'serverlessAWS'
     },
@@ -293,7 +302,7 @@ export const  certificates = [
       title: 'Go',
       certificates_image: 'img/go.jpg',
       description: 'Curso de go -Como programar y crear APIs Restfull con golang',
-      date: '2020-08-11T00:00:00.101Z',
+      date: new Date('2020-08-11T00:00:00.101Z'),
       institute: 'udemy',
       slug: 'udemygo'
     },
@@ -301,7 +310,7 @@ export const  certificates = [
       title: 'Redux',
       certificates_image: 'img/redux.jpg',
       description: 'React, Redux, Typescript, Firebase Fullstack Serveless',
-      date: '2020-08-05T00:00:00.101Z',
+      date: new Date('2020-08-05T00:00:00.101Z'),
       institute: 'udemy',
       slug: 'udemyredux'
     },
@@ -309,7 +318,7 @@ export const  certificates = [
       title: 'Css',
       certificates_image: 'img/varCssEDTeam.png',
       description: 'Variables Css',
-      date: '2018-10-23T00:00:00.101Z',
+      date: new Date('2018-10-23T00:00:00.101Z'),
       institute: 'EDTeam',
       slug: 'variablesCss'
     }
