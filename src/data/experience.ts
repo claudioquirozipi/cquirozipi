195500c42f35d5172ef30a13ebcc53756046af8b
Claudio Quiroz <claudioquirozipi@gmail.com>
2024-09-30 19:55:01 -0500

    fix bug image experiences

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -1,42 +1,43 @@
 export interface Experience {
-    title: string;
-    company: string;
-    location: string;
-    startDate: Date;
-    endDate: Date;
-    description: string;
-    logo?: string;
-    url?: string;
+  title: string;
+  company: string;
+  location: string;
+  startDate: Date;
+  endDate: Date;
+  description: string;
+  logo?: string;
+  url?: string;
 }
 
 export const experiences: Experience[] = [
-    {
-        title: 'Fullstack developer',
-        company: 'Nttdata',
-        location: 'Trujillo, Perú',
-        startDate: new Date("2020-09-20T05:00:00.000Z"),
-        endDate: new Date(),
-        description: 'Desarrollador de aplicaciones del lado del front y del backnend.',
-        logo: 'https://media.licdn.com/dms/image/D560BAQF3yEz9odOtlw/company-logo_100_100/0/1688376091927?e=1702512000&v=beta&t=3qod3JI71Vg5ujd_tXVw0iECbQ_CYGjzBpLKBgdax3M',
-        url: 'https://www.linkedin.com/company/nttdata/mycompany/verification/',
-    }, 
-    {
-        title: 'Frontend developer',
-        company: 'ID BI',
-        location: 'Lima, Perú',
-        startDate: new Date("2020-01-27T05:00:00.000Z"),
-        endDate: new Date("2020-06-30T05:00:00.000Z"),
-        description: 'Desarrollé un sistema de inventarios para restaurantes',
-        logo: 'https://media.licdn.com/dms/image/C4E0BAQGCWGYw7IMIbw/company-logo_100_100/0/1676065539172?e=1702512000&v=beta&t=O1rjS_kPKwxs8JxjcK8LDZaAbMqKEGXjmCTpTfhtxKk',
-        url: 'https://www.negocios.idbi.pe/',
-    }, 
-    {
-        title: 'Frontend developer',
-        company: 'Freelance',
-        location: 'Remote',
-        startDate: new Date('2017-01-10T05:00:00.000Z'),
-        endDate: new Date('2020-09-20T05:00:00.000Z'),
-        description: 'Desarrollé multiples landing pages para diferentes clientes',
-        
-    }
+  {
+    title: "Fullstack developer",
+    company: "Nttdata",
+    location: "Trujillo, Perú",
+    startDate: new Date("2020-09-20T05:00:00.000Z"),
+    endDate: new Date(),
+    description:
+      "Desarrollador de aplicaciones del lado del front y del backnend.",
+    logo: "img/experiences/nttdata.png",
+    url: "https://www.linkedin.com/company/nttdata/mycompany/verification/",
+  },
+  {
+    title: "Frontend developer",
+    company: "ID BI",
+    location: "Lima, Perú",
+    startDate: new Date("2020-01-27T05:00:00.000Z"),
+    endDate: new Date("2020-06-30T05:00:00.000Z"),
+    description: "Desarrollé un sistema de inventarios para restaurantes",
+    logo: "img/experiences/idbi.jpg",
+    url: "https://www.negocios.idbi.pe/",
+  },
+  {
+    title: "Frontend developer",
+    company: "Freelance",
+    location: "Remote",
+    startDate: new Date("2017-01-10T05:00:00.000Z"),
+    endDate: new Date("2020-09-20T05:00:00.000Z"),
+    description: "Desarrollé multiples landing pages para diferentes clientes",
+    logo: "img/experiences/wulpers.png",
+  },
 ];
