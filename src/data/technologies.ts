cc4d4235713f85aa5ce1fa859a782a3c0c43ccd9
Claudio Quiroz <claudioquirozipi@gmail.com>
2026-07-12 21:51:45 -0500

    update tecnologies

--- src/data/technologies.ts
+++ src/data/technologies.ts
@@ -2,129 +2,160 @@ export interface Technology {
     title: string;
     technologyImage: string;
     slug: string;
+    category?: string[];
   }
 
 export const technologies : Technology[]= [
-    { title: 'HYML', technologyImage: 'technologies/html.png', slug: '01html' },
-    { title: 'Css', technologyImage: 'technologies/css.jpg', slug: '02css' },
+    { title: 'HTML', technologyImage: 'technologies/html.png', slug: '01html',
+      category: ['frontend'] },
+    { title: 'Css', technologyImage: 'technologies/css.jpg', slug: '02css',
+      category: ['frontend'] },
     {
       title: 'Javascript',
       technologyImage: 'technologies/javascript.png',
-      slug: '03javascript'
+      slug: '03javascript',
+      category: ['lenguajes']
     },
     {
       title: 'Typescript',
       technologyImage: 'technologies/typescript.png',
-      slug: '04typescript'
+      slug: '04typescript',
+      category: ['lenguajes']
     },
-    { title: 'Git', technologyImage: 'technologies/git.png', slug: '05git' },
-    { title: 'Sass', technologyImage: 'technologies/sass.png', slug: '06sass' },
+    { title: 'Git', technologyImage: 'technologies/git.png', slug: '05git',
+      category: ['herramientas'] },
+    { title: 'Sass', technologyImage: 'technologies/sass.png', slug: '06sass',
+      category: ['frontend'] },
     {
       title: 'Bootstrap',
       technologyImage: 'technologies/bootstrap.png',
-      slug: '07bootstrap'
+      slug: '07bootstrap',
+      category: ['frontend']
     },
     {
       title: 'React',
       technologyImage: 'technologies/react.png',
-      slug: '08react'
+      slug: '08react',
+      category: ['frontend']
     },
     {
       title: 'Webpack',
       technologyImage: 'technologies/webpack.png',
-      slug: '09webpack'
+      slug: '09webpack',
+      category: ['herramientas']
     },
     {
       title: 'Styled Components',
       technologyImage: 'technologies/styledComponents.png',
-      slug: '10styledComponents'
+      slug: '10styledComponents',
+      category: ['frontend']
     },
     {
       title: 'Redux',
       technologyImage: 'technologies/redux.png',
-      slug: '11redux'
+      slug: '11redux',
+      category: ['frontend']
     },
     {
       title: 'Node.js',
       technologyImage: 'technologies/node.jpg',
-      slug: '12nodejs'
+      slug: '12nodejs',
+      category: ['backend']
     },
     {
       title: 'Python',
       technologyImage: 'technologies/python.png',
-      slug: '13python'
+      slug: '13python',
+      category: ['lenguajes']
     },
     {
       title: 'MongoDb',
       technologyImage: 'technologies/mongodb.jpg',
-      slug: '14mongoDb'
+      slug: '14mongoDb',
+      category: ['basededatos']
     },
     {
       title: 'GraphQL',
       technologyImage: 'technologies/graphql.png',
-      slug: '15graphQL'
+      slug: '15graphQL',
+      category: ['herramientas']
     },
     {
       title: 'Firebase',
       technologyImage: 'technologies/firebase.png',
-      slug: '16firebase'
+      slug: '16firebase',
+      category: ['cloud']
     },
     {
       title: 'Gatsby',
       technologyImage: 'technologies/gatsby.png',
-      slug: '17gatsby'
+      slug: '17gatsby',
+      category: ['frontend']
     },
     {
       title: 'Next.js',
       technologyImage: 'technologies/nextjs.svg',
-      slug: '18nextjs'
+      slug: '18nextjs',
+      category: ['frontend']
     },
     {
       title: 'Formik',
       technologyImage: 'technologies/formik.png',
-      slug: '19formik'
+      slug: '19formik',
+      category: ['frontend']
     },
-    { title: 'Deno', technologyImage: 'technologies/deno.png', slug: '20deno' },
+    { title: 'Deno', technologyImage: 'technologies/deno.png', slug: '20deno',
+      category: ['backend'] },
     {
       title: 'Golang',
       technologyImage: 'technologies/golang.png',
-      slug: '21golang'
+      slug: '21golang',
+      category: ['lenguajes']
     },
     {
       title: 'Svelte',
       technologyImage: 'technologies/svelte.png',
-      slug: '22svelte'
+      slug: '22svelte',
+      category: ['frontend']
     },
     {
       title: 'Strapi',
       technologyImage: 'technologies/strapi.svg',
-      slug: '23strapi'
+      slug: '23strapi',
+      category: ['backend']
     },
     {
       title: 'Material-ui',
       technologyImage: 'technologies/material-ui.svg',
-      slug: '24materialui'
+      slug: '24materialui',
+      category: ['frontend']
     },
     {
       title: 'Nest.js',
       technologyImage: 'technologies/nestjs.svg',
-      slug: 'css copy 2'
+      slug: 'css copy 2',
+      category: ['backend']
     },
-    { title: 'Lit', technologyImage: 'technologies/lit.png', slug: '25lit' },
+    { title: 'Lit', technologyImage: 'technologies/lit.png', slug: '25lit',
+      category: ['frontend'] },
     {
       title: 'Polymer',
       technologyImage: 'technologies/polymer.svg',
-      slug: '26polymer'
+      slug: '26polymer',
+      category: ['frontend']
     },
-    { title: 'Java', technologyImage: 'technologies/java.png', slug: '27java' },
+    { title: 'Java', technologyImage: 'technologies/java.png', slug: '27java',
+      category: ['lenguajes'] },
     {
       title: 'Google Cloud Platform',
       technologyImage: 'technologies/gcp.png',
-      slug: '28gcp'
+      slug: '28gcp',
+      category: ['cloud']
     },
     {
       title: 'Cells',
       technologyImage: 'technologies/open-cells.svg',
-      slug: '29cells'
+      slug: '29cells',
+      category: ['frontend']
     }
   ]
