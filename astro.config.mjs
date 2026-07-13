75b2dea9b57c90ca25a290e7390b5363b833e9a7
Claudio Quiroz <claudioquirozipi@gmail.com>
2026-07-12 23:30:58 -0500

    sitemap and robot.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://cquirozipi.vercel.app",
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }
